a7c5c2fde38b0550e27c0caf2a4aaff5d606b839
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-03-31 10:52:42 -0300

    feat(files): remove .next web folder from git track

--- web/src/layout/components/header/index.tsx
+++ web/src/layout/components/header/index.tsx
@@ -2,6 +2,9 @@ import { useTheme } from '@/context/theme'
 import { ThemeSwitcher } from '../theme-switcher'
 import * as Styles from './styles'
 import { appSettings } from '@/config/app'
+import { paths } from '@/constants/routes'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export function Header () {
   const { currentTheme, toggleTheme } = useTheme()
@@ -9,9 +12,11 @@ export function Header () {
   const defaultChecked = currentTheme !== appSettings.theme.darkMode
   const checked = currentTheme === appSettings.theme.lightMode
 
-  console.log(checked)
   return (
     <Styles.Container>
+      <Link href={paths.home}>
+        <Image src="/logo-sm.svg" alt="Logo" width={150} height={40} style={{ objectFit: 'contain' }} />
+      </Link>
       <ThemeSwitcher
         checked={checked} 
         theme={currentTheme}
--- web/src/layout/components/header/styles.ts
+++ web/src/layout/components/header/styles.ts
@@ -2,7 +2,7 @@ import { styled } from 'stitches.config'
 
 export const Container = styled('header', {
   display: 'flex',
-  justifyContent: 'flex-end',
+  justifyContent: 'space-between',
   alignItems: 'center',
   padding: '0.5rem 1rem',
   background: '$foreground',
